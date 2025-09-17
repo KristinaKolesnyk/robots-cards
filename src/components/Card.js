29da9708bd196e1dc2b7f3aa3d716e10e78902d7
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-09-17 18:01:09 -0400

    readme

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { buildRobotMeta } from "../utils/legend";
+import {buildRobotMeta} from "../utils/legend";
 
-const Card = ({ user, inCrew, onView, onToggleCrew }) => {
-    const { origin, rank, stars, specialty } = buildRobotMeta(user);
+const Card = ({user, inCrew, onView, onToggleCrew}) => {
+    const {origin, rank, stars, specialty} = buildRobotMeta(user);
 
     return (
         <div className="tc dib br3 pa3 ma2 shadow-5 robo-card">
@@ -15,13 +15,7 @@ const Card = ({ user, inCrew, onView, onToggleCrew }) => {
             />
             <div className="mt2">
                 <h2 className="f4 mt2 mb1">{user.name}</h2>
-                <div className="badges">
-                    <span className="badge" title="Origin">{origin}</span>
-                    <span className="badge" title="Rank">
-            {rank} {"★".repeat(stars)}
-          </span>
-                    <span className="badge" title="Specialty">{specialty}</span>
-                </div>
+                
 
                 <div className="mt3 flex justify-center gap8">
                     <button className="btn" onClick={() => onView(user)}>View Passport</button>
